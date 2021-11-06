fb520afe67a1feb501e10ae22884ba3cdd5c486a
ArtfulAlpha <artfulalpha@gmail.com>
2021-11-06 02:15:50 +0200

    Рефакторинг кода

--- main.js
+++ main.js
@@ -1,49 +1,66 @@
+
+
 document.querySelector('.get-result').onclick = getResult;
-
-let error = "Error: Указаны не все параметры, или результат не равен числу.";
-
+const error = "Error: Указаны не все параметры, или результат не равен числу.";
 function getResult() {
+  
   let resultCalc;
-  getValueOneOperand();
-  getValueTwoOperand();
-  getChoiseOperation();
+  const oneOperand = getValueOneOperand();
+  const twoOperand = getValueTwoOperand();
+  const operation = getChoiseOperation();
 
-  if ((getValueOneOperand() !== NaN) && (getValueTwoOperand() !== NaN)) {
+  if (!(oneOperand === null) && !(twoOperand === null)) {
 
     switch (getChoiseOperation()) {
       case "+":
-        resultCalc = getValueOneOperand() + getValueTwoOperand();
+        resultCalc = oneOperand + twoOperand;
         break;
       case "-":
-        resultCalc = getValueOneOperand() - getValueTwoOperand();
+        resultCalc = oneOperand - twoOperand;
         break;
       case "/":
-        resultCalc = getValueOneOperand() / getValueTwoOperand();
+        resultCalc = oneOperand / twoOperand;
         break;
       case "%":
-        resultCalc = getValueOneOperand() % getValueTwoOperand();
+        resultCalc = oneOperand % twoOperand;
         break;
       case "**":
-        resultCalc = getValueOneOperand() ** getValueTwoOperand();
+        resultCalc = oneOperand ** twoOperand;
         break;
       default:
         resultCalc = "unknown operation";
     }
   } else {
     resultCalc = error;
-  };
-  writeResult(resultCalc);
-
-
-
+  }; 
+  writeResult(checkResult(resultCalc));
 };
+function checkResult(resultCalc) {
+  if ((resultCalc === Infinity) || isNaN(resultCalc) ||(resultCalc === -Infinity) ) {
+    return resultCalc = error;
+  } else {
+    return resultCalc;
+  }
+}
 
 function getValueOneOperand() {
-  return valueOperandOne = +document.querySelector(".operand-one").value
+  valueOperandOne = document.querySelector(".operand-one").value;
+  if (valueOperandOne === "") {
+    return null
+  } else {
+
+    return Number(valueOperandOne);
+  }
 };
 
 function getValueTwoOperand() {
-  return valueOperandTwo = +document.querySelector(".operand-two").value
+  valueOperandTwo = document.querySelector(".operand-two").value;
+  if (valueOperandTwo === "") {
+    return null
+
+  } else {
+    return Number(valueOperandTwo);
+  }
 };
 
 function getChoiseOperation() {
